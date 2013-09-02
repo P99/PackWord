f2422e63685b4c70817cc0126da410c6357ade3c
P99 <P99_Pascal@yahoo.fr>
2013-09-02 15:24:54 +0200

    Changed host and port to bind websocket server to the ouside world

--- php/ws-server.php
+++ php/ws-server.php
@@ -15,7 +15,7 @@ require(__DIR__ . '/Wrench/lib/SplClassLoader.php');
 $classLoader = new SplClassLoader('Wrench', __DIR__ . '/Wrench/lib');
 $classLoader->register();
 
-$server = new \Wrench\Server('ws://' . $_ENV['OPENSHIFT_APP_DNS'] . ':8000', array(
+$server = new \Wrench\Server('ws://' . $_ENV['OPENSHIFT_WS_IP'] . ':' . $_ENV['OPENSHIFT_WS_PORT'], array(
     'allowed_origins'            => array(
         'mysite.localhost'
     ),
